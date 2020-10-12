d6dc1e2d83df4826e731490096965bb1e1728ced
Jackson Silva <jackson.victorius@gmail.com>
2020-10-11 21:38:17 -0300

    Lorena Randomizer - OK!

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const {regularPhrases, startingPhrases, endingPhrases} = require('./dataset/data
 
 function main() {
     const phraseType = process.argv[2];
-    if(!phraseType) return;
+    if(!phraseType) {
+        console.error("ERROR: Missing Phrase Type Argument.\n(0) => Lorena-English\n(1) => English\n(2) => Portuguese\n\n\tExample: npm start 1\n\n** Optional secondary argument # of phrases **");
+        return;
+    }
 
-    const maxPossibilities = regularPhrases.length + !!startingPhrases.length + !!endingPhrases;
+    const maxPossibilities = regularPhrases.length + !!startingPhrases + !!endingPhrases - 1;
+    console.log('maxPossibilities', maxPossibilities);
 
     const phrasesToPick = process.argv[3] || 1;
     let randomPhrasesSet = new Array();
     let times = 0;
 
     while (randomPhrasesSet.length < phrasesToPick && randomPhrasesSet.length < maxPossibilities) {
-        let randomPhrase = GetRandomSet(!!randomPhrasesSet.length, randomPhrasesSet.length == phrasesToPick);
+        let randomPhrase = GetRandomSet(!randomPhrasesSet.length, randomPhrasesSet.length == phrasesToPick - 1);
 
         if (!randomPhrasesSet.includes(randomPhrase))
             randomPhrasesSet.push(randomPhrase);
@@ -25,9 +29,13 @@ function main() {
 }
 
 function GetRandomSet(isStart, isEnd) {
-    let randomNum = Math.floor(Math.random() * regularPhrases.length);
+    let datasets = [startingPhrases, regularPhrases, endingPhrases];
+    let currentDataset = isStart ? 0 : isEnd ? 2 : 1;
+    console.log("Current Dataset:", currentDataset);
+    let dataset = datasets[currentDataset];
+    let randomNum = Math.floor(Math.random() * dataset.length);
 
-    return regularPhrases[randomNum];
+    return dataset[randomNum];
 }
 
 
